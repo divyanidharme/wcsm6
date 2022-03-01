e37cf5441fb052c8abbfe6a15caf377717b83239
abhij <abhij@LAPTOP-RTQNQKLG>
2022-03-01 08:56:22 +0530

    my first commit of framework

---
+++ testActiTime/src/generic/IAutoConstant.java
@@ -0,0 +1,13 @@
+package generic;
+
+public interface IAutoConstant {
+	public String CHROME_KEY="webdriver.chrome.driver";
+	public String CHROME_PATH="./drivers/chrome/geckodriver.exe";
+	
+	public String GECKO_KEY="webdriver.gecho.driver";
+	public String GECKO_PATH="./driver/gecho.driver.exe";
+	
+	public String EXCEL_PATH="./data/TestData1.xlsx";
+	public String PROP_PATH="./data/TestData1.xlsx";
+
+}
---
+++ testActiTime/src/testPackage/TestActiTimeInvalidLogin.java
@@ -0,0 +1,18 @@
+package testPackage;
+
+import org.junit.Test;
+
+import generic.BaseTest;
+
+public class TestActiTimeInvalidLogin  extends BaseTest {
+	
+	@Test
+	public void testActiTimeInvalidlogin()
+	{
+		
+		
+	}
+	
+	
+
+}
